ecae87191f0a23f99d3084eda8fa6531f75567c4
Konstantin Makarov <hippik80@gmail.com>
2024-09-20 15:26:53 +0300

    Mocks refactor

--- internal/listener/listener_mocks_test.go
+++ internal/listener/listener_mocks_test.go
@@ -2,10 +2,55 @@ package listener
 
 import (
 	"context"
-
 	"github.com/jackc/pgx"
+	"time"
+
 	"github.com/stretchr/testify/mock"
+
+	trx "github.com/ihippik/wal-listener/v2/internal/listener/transaction"
+	"github.com/ihippik/wal-listener/v2/internal/publisher"
 )
+
+type monitorMock struct{}
+
+func (m *monitorMock) IncPublishedEvents(subject, table string) {}
+
+func (m *monitorMock) IncFilterSkippedEvents(table string) {}
+
+func (m *monitorMock) IncProblematicEvents(kind string) {}
+
+type parserMock struct {
+	mock.Mock
+}
+
+func (p *parserMock) ParseWalMessage(msg []byte, tx *trx.WAL) error {
+	args := p.Called(msg, tx)
+	now := time.Now()
+
+	tx.BeginTime = &now
+	tx.CommitTime = &now
+	tx.Actions = []trx.ActionData{
+		{
+			Schema: "public",
+			Table:  "users",
+			Kind:   "INSERT",
+			NewColumns: []trx.Column{
+				trx.InitColumn(nil, "id", 1, 23, true),
+			},
+		},
+	}
+
+	return args.Error(0)
+}
+
+type publisherMock struct {
+	mock.Mock
+}
+
+func (p *publisherMock) Publish(ctx context.Context, subject string, event *publisher.Event) error {
+	args := p.Called(ctx, subject, event)
+	return args.Error(0)
+}
 
 type replicatorMock struct {
 	mock.Mock
--- internal/listener/monitor_mock_test.go
+++
@@ -1,9 +0,0 @@
-package listener
-
-type monitorMock struct{}
-
-func (m *monitorMock) IncPublishedEvents(subject, table string) {}
-
-func (m *monitorMock) IncFilterSkippedEvents(table string) {}
-
-func (m *monitorMock) IncProblematicEvents(kind string) {}
